1c1acd724ef95236cb18e3a6fc73f2faaf644eaa
airtysay <105341001+airtysay@users.noreply.github.com>
2022-09-19 09:52:46 +0900

    v1.1.0 lotto 기능구현
    하지만 텍스트 박스에서 초기화가 안되고 같은 결과값이 여러개가 출력 되는 현상 이 있다.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,17 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("맑은 고딕", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(60, 24);
+            this.label1.Location = new System.Drawing.Point(31, 24);
             this.label1.MaximumSize = new System.Drawing.Size(500, 300);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(448, 86);
@@ -43,11 +47,63 @@
             this.label1.Text = "Lotto Number";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnClickThis.Location = new System.Drawing.Point(105, 323);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(113, 45);
+            this.btnClickThis.TabIndex = 1;
+            this.btnClickThis.Text = "ClickThis";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnEnd.Location = new System.Drawing.Point(301, 323);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(113, 45);
+            this.btnEnd.TabIndex = 2;
+            this.btnEnd.Text = "End";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(192, 294);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(99, 124);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 150);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(568, 450);
+            this.ClientSize = new System.Drawing.Size(505, 390);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnClickThis);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -59,6 +115,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+
 
 namespace lotto
 {
     public partial class Form1 : Form
     {
+        int repeat;
+        string text;
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +23,54 @@ namespace lotto
 
         private void label1_Click(object sender, EventArgs e)
         {
+            string conmStr = @"Data Source=C:\Users\Owner\Desktop\lottodb.db";
+            string stm = "SELECT SQLITE_VERSION()";
 
+            var con = new SQLiteConnection(conmStr);
+            con.Open();
+
+            var cmd = new SQLiteCommand(stm, con);
+            string version = cmd.ExecuteScalar().ToString();
+
+            Console.WriteLine(version);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string conmStr = @"Data Source=C:\Users\Owner\Documents\GitHub\lotto2\lottodb.db";
+            var con = new SQLiteConnection(conmStr);
+                con.Open();
+            for(int i = 0; i <= repeat; i++)
+            {
+                string Number;
+                Random random = new Random();
+                int[] RandomArray = new int[6];
+                for(int j = 0; j < 6; j++) {
+                    RandomArray[j] = random.Next(1, 45);
+                }
+                Number = String.Join(",", RandomArray);
+                String sql = "insert into LottoNumber (LottoNumber) values('" + Number + "')";
+                text += Number + "\r\n";
+                SQLiteCommand command = new SQLiteCommand(sql,con);
+                Console.WriteLine(sql);
+
+                int result = command.ExecuteNonQuery();
+            }
+            textBox1.Text = text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            repeat = comboBox1.SelectedIndex;  
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
